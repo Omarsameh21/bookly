68a04f66a676103cd298afafee0ddf25eefb1271
omarsameh21 <omarsamehomarsenjab240@gmail.com>
2024-10-27 23:05:29 +0300

    create SplashViewBody

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/Features/splash/presentation/view/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
