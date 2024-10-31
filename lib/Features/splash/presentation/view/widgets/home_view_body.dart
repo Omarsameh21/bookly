5172add5c96735d8e98a6f28109dd5d98d08d5a2
omarsameh21 <omarsamehomarsenjab240@gmail.com>
2024-10-31 21:54:23 +0300

    create Styles file & add title to HomeView

--- lib/Features/splash/presentation/view/widgets/home_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
 import 'costum_app_bar.dart';
 import 'featured_books_list_view.dart';
 
@@ -8,11 +9,22 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CostumAppBar(),
         FeaturedBooksListView(),
+        Padding(
+          padding: EdgeInsets.only(
+            top: 50,
+            left: 16,
+            bottom: 24,
+          ),
+          child: Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        )
       ],
     );
   }
 }
-
