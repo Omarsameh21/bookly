a29406fbf42d3ecf8f87999092afce8fe79c1f14
omarsameh21 <omarsamehomarsenjab240@gmail.com>
2024-11-01 00:09:33 +0200

    add some Styles to app & create part of BestSellerListViewItem

--- lib/Features/splash/presentation/view/widgets/home_view_body.dart
+++ lib/Features/splash/presentation/view/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
 import 'costum_app_bar.dart';
 import 'featured_books_list_view.dart';
@@ -17,14 +18,69 @@ class HomeViewBody extends StatelessWidget {
           padding: EdgeInsets.only(
             top: 50,
             left: 16,
-            bottom: 24,
+            bottom: 20,
           ),
           child: Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
-        )
+        ),
+        BestSellerListViewitem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewitem extends StatelessWidget {
+  const BestSellerListViewitem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 125,
+            child: AspectRatio(
+              aspectRatio: 1.5 / 2.4,
+              child: Container(
+                width: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  image: const DecorationImage(
+                    image: AssetImage(
+                      AssetData.testImage,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: const Text(
+                    'Harry Potter And The Goblet Of Fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle20,
+                  ),
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/splash/presentation/view/splash_view.dart';
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Bookly());
@@ -13,7 +14,11 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          )),
       home: const SplashView(),
     );
   }
